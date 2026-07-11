ac7f8cec316375853a68829532ee224d6306c3fd
Ethan <kaleo.e.ching@gmail.com>
2026-07-11 16:34:33 +0800

    Require all prerequisites for availability

--- tests/test_planning.py
+++ tests/test_planning.py
@@ -22,3 +22,13 @@ def test_find_available_courses_excludes_completed_courses():
     available = find_available_courses(courses, {"CS 18000"})
 
     assert [course.code for course in available] == ["CS 18200"]
+
+
+def test_find_available_courses_requires_all_prerequisites():
+    courses = [
+        Course("CS 24000", "Programming in C", 3, "core", ["CS 18000", "CS 18200"]),
+    ]
+
+    available = find_available_courses(courses, {"CS 18000"})
+
+    assert available == []
