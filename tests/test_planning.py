828fb8862fd899642d84793e70f5a986095b6226
Ethan <kaleo.e.ching@gmail.com>
2026-07-11 16:17:10 +0800

    Exclude completed courses from availability

--- tests/test_planning.py
+++ tests/test_planning.py
@@ -11,3 +11,14 @@ def test_find_available_courses_excludes_locked_courses():
     available = find_available_courses(courses, set())
 
     assert [course.code for course in available] == ["CS 18000"]
+
+
+def test_find_available_courses_excludes_completed_courses():
+    courses = [
+        Course("CS 18000", "Problem Solving", 4, "core"),
+        Course("CS 18200", "Foundations", 3, "core", ["CS 18000"]),
+    ]
+
+    available = find_available_courses(courses, {"CS 18000"})
+
+    assert [course.code for course in available] == ["CS 18200"]
