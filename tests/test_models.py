25a1f4ef4551b556f1112a72865ae20c2d0be8ae
Ethan <kaleo.e.ching@gmail.com>
2026-07-11 12:13:37 +0800

    Test course credit validation

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,5 @@
+import pytest
+
 from degree_planner.models import Course
 
 
@@ -6,3 +8,8 @@ def test_course_stores_basic_fields():
 
     assert course.code == "CS 18000"
     assert course.credits == 4
+
+
+def test_course_rejects_zero_credits():
+    with pytest.raises(ValueError):
+        Course("CS 18000", "Problem Solving", 0, "core")
