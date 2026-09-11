b831314a64d15ff5b9284543bd11713619240b28
Ethan <kaleo.e.ching@gmail.com>
2026-09-11 20:08:55 +0800

    Test valid prerequisite graph

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -8,3 +8,12 @@ def test_find_missing_prerequisites_returns_unknown_prerequisite_codes():
     ]
 
     assert find_missing_prerequisites(courses) == ["CS 18000"]
+
+
+def test_find_missing_prerequisites_returns_empty_list_when_all_exist():
+    courses = [
+        Course("CS 18000", "Problem Solving", 4, "core"),
+        Course("CS 18200", "Foundations", 3, "core", ["CS 18000"]),
+    ]
+
+    assert find_missing_prerequisites(courses) == []
